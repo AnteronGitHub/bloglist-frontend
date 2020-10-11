e910e8778226d3625d6ba67d1953980afa3487f4
Antero Vainio <anterongugl@gmail.com>
2020-10-11 16:15:16 +0300

    5.7: Enable displaying detailed information of blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,48 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailed, setDetailed] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetailed = e => {
+    setDetailed(!detailed)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {detailed
+      ? (
+        <>
+        <div>
+          {blog.title}{' '}
+          <button onClick={toggleDetailed}>hide</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes: {blog.likes}
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+        </>
+      )
+      : (
+        <div>
+          {blog.title} {blog.author}{' '}
+          <button onClick={toggleDetailed}>view</button>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
