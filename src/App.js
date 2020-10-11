12d32eea3443ad8e8ea62edf8bfed844f8ff609b
Antero Vainio <anterongugl@gmail.com>
2020-10-11 13:15:09 +0300

    5.1: Add login form for unauthenticated users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUsername('')
+      setPassword('')
+      setUser(user)
+    } catch (exception) {
+      console.error(exception)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username:{' '}
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password:{' '}
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +57,19 @@ const App = () => {
     )  
   }, [])
 
+  if (!user) {
+    return <LoginForm setUser={token => setUser(token)} />
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <div>{user.username} logged in</div><br />
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
     </div>
   )
 }
