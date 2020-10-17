922686d29fc8f4ecbef0f2faad582c1c660c5603
Antero Vainio <anterongugl@gmail.com>
2020-10-17 18:41:07 +0300

    5.18: Add test cases for logging in

--- cypress/integration/blogapp.spec.js
+++ cypress/integration/blogapp.spec.js
@@ -1,10 +1,32 @@
 describe('Blog app', function() {
+  const testUser = {
+    username: "testuser",
+    password: "testpass"
+  }
   beforeEach(function() {
     cy.request('POST', `${Cypress.env('apiUrl')}/api/testing/reset`)
+    cy.request('POST', `${Cypress.env('apiUrl')}/api/users`, testUser)
     cy.visit(`${Cypress.env('baseUrl')}/`)
   })
 
   it('Login from is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type(testUser.username)
+      cy.get('input:last').type(testUser.password)
+      cy.contains('login').click()
+      cy.contains(`${testUser.username} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type(testUser.username)
+      cy.get('input:last').type('rand')
+      cy.contains('login').click()
+      cy.contains('invalid username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
