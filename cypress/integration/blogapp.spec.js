894210f00847ba41e5d0341312ac4b8fe5b16a93
Antero Vainio <anterongugl@gmail.com>
2020-10-17 19:17:42 +0300

    5.21: Add test cases to confirm that only the user who added a blog can remove it

--- cypress/integration/blogapp.spec.js
+++ cypress/integration/blogapp.spec.js
@@ -54,13 +54,13 @@ describe('Blog app', function() {
     })
 
     describe('and a blog exists', function () {
-      beforeEach(function () {
-        const testBlog = {
-          title: "test blog",
-          author: "test author",
-          url: "http://example.com"
-        }
+      const testBlog = {
+        title: "test blog",
+        author: "test author",
+        url: "http://example.com"
+      }
 
+      beforeEach(function () {
         cy.contains('create').click()
         cy.get('#title').type(testBlog.title)
         cy.get('#author').type(testBlog.author)
@@ -73,7 +73,24 @@ describe('Blog app', function() {
         cy.contains('like').click()
         cy.contains('likes: 1')
       })
-    })
 
+      it('A blog can be removed by the user who added it', function() {
+        cy.contains('view').click()
+        cy.contains('remove').click()
+        cy.contains(`Removed ${testBlog.title} by ${testBlog.author}`)
+      })
+
+      it('A blog cannot be removed by a user who did not add it', function() {
+        const testUser2 = {
+          username: "testuser2",
+          password: "testpass2"
+        }
+        cy.request('POST', `${Cypress.env('apiUrl')}/api/users`, testUser2)
+        cy.login(testUser2)
+        cy.contains('view').click()
+        cy.contains('remove').click()
+        cy.contains('no matching blogs for authenticated users was found')
+      })
+    })
   })
 })
